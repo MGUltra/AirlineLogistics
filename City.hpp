3f037f92b198c2935517897709ce82875cb1f570
matt <matthewhgarner@gmail.com>
2019-03-22 14:40:51 -0700

    city class update

--- City.hpp
+++ City.hpp
@@ -6,6 +6,39 @@
 #include <iostream>
 #include <string>
 
+class City
+{
+private:
 
+	std::string airportName;
+	std::string cityName;
+	std::string countryName;
+
+	float longitude;
+	float latitude;
+
+	int latDegrees, latMinutes, latSeconds;
+	char latDirection // N or S
+
+
+	int longDegrees, longMinutes, longSeconds;
+	char longDirection; // E or W
+
+
+
+	int altitude;
+
+
+public:
+	
+	City();
+	~City();
+
+	// Set Functions
+
+
+	// Get Functions
+
+}
 
 #endif
