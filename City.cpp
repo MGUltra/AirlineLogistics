8ed8d4350395083c99eb96a4bd006cd5c7c9c74b
matt <matthewhgarner@gmail.com>
2019-03-22 20:07:37 -0400

    added initial City class member variables and functions

--- City.cpp
+++ City.cpp
@@ -1,1 +1,189 @@
 #include "City.hpp"
+
+
+
+/*******************************************************
+*                     CONSTRUCTOR
+********************************************************/
+City::City()
+{
+
+}
+
+/*******************************************************
+*                     DESTRUCTOR
+********************************************************/
+City::~City()
+{
+
+}
+
+
+
+/*******************************************************
+*                     SET FUNCTIONS
+********************************************************/
+
+
+void City::setAirportName(std::string)
+{
+
+}
+
+void City::setCityName(std::string)
+{
+
+}
+
+void City::setCountryName(std::string)
+{
+
+}
+
+void City::setLongitude(float)
+{
+
+}
+
+void City::setLatitude(float)
+{
+
+}
+
+void City::setLatDegrees(int)
+{
+
+}
+
+void City::setLatMinutes(int)
+{
+
+}
+
+void City::setLatSeconds(int)
+{
+
+}
+
+void City::setLongDegrees(int)
+{
+
+}
+
+void City::setLongMinutes(int)
+{
+
+}
+
+void City::setLongSeconds(int)
+{
+
+}
+
+void City::setLatDirection(char)
+{
+
+}
+
+void City::setLongDirection(char)
+{
+
+}
+
+void City::setAltitude(int)
+{
+
+}
+
+
+/*******************************************************
+*                     GET FUNCTIONS
+********************************************************/
+
+
+std::string City::getAirportName()
+{
+
+}
+
+
+std::string City::getCityName()
+{
+
+}
+
+
+std::string City::getCountryName()
+{
+
+}
+
+
+float City::getLongitude()
+{
+
+}
+
+
+float City::getLatitude()
+{
+
+}
+
+
+int City::getLatDegrees()
+{
+
+}
+
+
+int City::getLatMinutes()
+{
+
+}
+
+
+int City::getLatSeconds()
+{
+
+}
+
+
+int City::getLongDegrees()
+{
+
+}
+
+
+int City::getLongMinutes()
+{
+
+}
+
+
+int City::getLongSeconds()
+{
+
+}
+
+
+char City::getLatDirection()
+{
+
+}
+
+
+char City::getLongDirection()
+{
+
+}
+
+
+int City::getAltitude()
+{
+
+}
+
+
+
+
--- City.hpp
+++ City.hpp
@@ -14,8 +14,8 @@ private:
 	std::string cityName;
 	std::string countryName;
 
-	float longitude;
-	float latitude;
+	 longitude;
+	 latitude;
 
 	int latDegrees, latMinutes, latSeconds;
 	char latDirection // N or S
@@ -35,10 +35,36 @@ public:
 	~City();
 
 	// Set Functions
-
+	void setAirportName(std::string);
+	void setCityName(std::string);
+	void setCountryName(std::string);
+	void setLongitude();
+	void setLatitude();
+	void setLatDegrees(int);
+	void setLatMinutes(int);
+	void setLatSeconds(int);
+	void setLongDegrees(int);
+	void setLongMinutes(int);
+	void setLongSeconds(int);
+	void setLatDirection(char);
+	void setLongDirection(char);
+	void setAltitude(int);
 
 	// Get Functions
-
+	std::string getAirportName();
+	std::string getCityName();
+	std::string getCountryName();
+	float getLongitude();
+	float getLatitude();
+	int getLatDegrees();
+	int getLatMinutes();
+	int getLatSeconds();
+	int getLongDegrees();
+	int getLongMinutes();
+	int getLongSeconds();
+	char getLatDirection();
+	char getLongDirection();
+	int getAltitude();
 }
 
 #endif
