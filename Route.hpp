0d35068727fb95f64f88a068b5f52d741f8f9347
matt <matthewhgarner@gmail.com>
2019-03-24 10:46:27 -0400

    updated route functions

--- Route.hpp
+++ Route.hpp
@@ -14,12 +14,24 @@ class Route
 private:
 	Airport* origin;
 	Airport* destination;
-
 	int distance;
 
-
 public:
+	Route();
+	~Route();
 
+	// Set Functions
+
+	void setOrigin(Airport*);
+	void setDestination(Airport*);
+	void setDistance(int);
+
+	// Get Functions
+
+	Airport* getOrigin();
+	Airport* getDestination();
+	
+	int getDistance();
 
 
 };
